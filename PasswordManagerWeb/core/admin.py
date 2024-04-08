3d81258259c170717459b151a6d4e4703c6f033e
al3xHub <alejandrobueno92@hotmail.com>
2024-04-08 16:36:15 +0200

    modelo site y migración

--- PasswordManagerWeb/core/admin.py
+++ PasswordManagerWeb/core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Site
+
+admin.site.site_header = "ADMIN DASHBOARD: Password Manager"
+
+
+class AdminConfig(admin.ModelAdmin):
+    list_display = ('website_name', 'created')
+    exclude = ('website_password', 'master_password',)
+
 
 # Register your models here.
+admin.site.register(Site, AdminConfig)
--- PasswordManagerWeb/core/models.py
+++ PasswordManagerWeb/core/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.urls import reverse
+
 
 # Create your models here.
+class Site(models.Model):
+    website_name = models.CharField(max_length=50)
+    website_link = models.URLField(max_length=200, null=True)
+    website_username = models.CharField(max_length=50)
+    website_password = models.CharField(max_length=50)
+    website_notes = models.CharField(max_length=50, null=True)
+    master_password = models.CharField(max_length=200, default='')
+
+    created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.website_name
+
+    def get_absolute_url(self):
+        return reverse("home")
